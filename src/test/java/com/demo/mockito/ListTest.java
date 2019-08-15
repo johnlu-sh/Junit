9cba38b867afd79c4b08782d1930f0f5e0986cfe
John Lu <john.lu01@sap.com>
2019-08-15 09:53:30 +0800

    exercise 3

---
+++ src/test/java/com/demo/mockito/ListTest.java
@@ -0,0 +1,44 @@
+package com.demo.mockito;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+import org.junit.Test;
+
+public class ListTest {
+
+  @Test
+  public void mockListSize() {
+    List list = mock(List.class);
+    when(list.size()).thenReturn(10);
+    assertEquals(10, list.size());
+  }
+
+  @Test
+  public void mockListSizeWithMultipleReturnValues() {
+    List list = mock(List.class);
+    when(list.size()).thenReturn(10).thenReturn(20);
+    assertEquals(10, list.size());  // First call
+    assertEquals(20, list.size());  // Second call
+  }
+
+  @Test
+  public void mockListGet() {
+    List<String> list = mock(List.class);
+    when(list.get(0)).thenReturn("mockito");
+    assertEquals("mockito", list.get(0));
+    assertNull(list.get(1));
+  }
+
+  @Test
+  public void mockGetWithAny() {
+    List<String> list = mock(List.class);
+    when(list.get(anyInt())).thenReturn("mockito");   // Argument Matchers
+    assertEquals("mockito", list.get(0));
+    assertEquals("mockito", list.get(1));
+  }
+}
